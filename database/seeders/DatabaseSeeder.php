2114ca24dafa5eb3505de7958caab4fe59728284
Alvin Budi H <alvinbudihartono123@gmail.com>
2022-06-11 15:56:15 +0700

    mengisi data user, role dan user_role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $roles = ["Admin", "Kasir", "Pelayanan"];
+
+        for ($i = 0; $i < count($roles); $i++) {
+            User::create([
+                "name" => $roles[$i] . " PKB",
+                "username" => strtolower($roles[$i]),
+                "email" => strtolower($roles[$i]) . "@gmail.com",
+                "password" => Hash::make("password")
+            ]);
+
+            Role::create([
+                "role_name" => $roles[$i]
+            ]);
+
+            UserRole::create([
+                "user_id" => $i + 1,
+                "role_id" => $i + 1
+            ]);
+        }
     }
 }
